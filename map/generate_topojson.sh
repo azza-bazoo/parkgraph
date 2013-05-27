9de8d7f8ef75df897ac1458ade0a07939b59cbfa
Hourann Bosci <hourannb@dview.net>
2013-05-27 21:32:38 +0800

    update generated map data: include buildings, separate parks from waterways, and use different clipping bounds (to suit new map scaling)

--- map/generate_topojson.sh
+++ map/generate_topojson.sh
@@ -13,15 +13,25 @@ if [ ! -e shape/roads.shp ]
   then echo "no shapefiles? download and unzip Perth.osm.shp.zip from http://download.bbbike.org/osm/"; exit 1
 fi
 
-echo "ogr2ogr on roads.shp"
-ogr2ogr -f GeoJSON -clipdst 115.83 -31.93 115.90 -31.98 roads.json shape/roads.shp
 
-echo "ogr2ogr on waterways.shp"
-ogr2ogr -f GeoJSON -clipdst 115.83 -31.93 115.90 -31.98 waterways.json shape/waterways.shp
+echo "ogr2ogr on roads.shp"
+ogr2ogr -f GeoJSON -where "type IN ('primary', 'secondary', 'tertiary', 'residential', 'motorway', 'trunk', 'primary_link', 'motorway_link', 'trunk_link', 'unclassified')" -clipdst 115.837 -31.92 115.891 -31.99 roads.json shape/roads.shp
+
+echo "ogr2ogr on natural.shp (looking for parks)"
+ogr2ogr -f GeoJSON -where "type = 'park'" -clipdst 115.837 -31.92 115.891 -31.99 parks.json shape/natural.shp
+
+echo "ogr2ogr on natural.shp (looking for water)"
+ogr2ogr -f GeoJSON -where "type IN ('water', 'riverbank')" -clipdst 115.837 -31.92 115.891 -31.99 water.json shape/natural.shp
+
+# use tighter clip bounds on buildings (Vincent St to the Narrows); we really only care about the CBD
+echo "ogr2ogr on buildings.shp"
+ogr2ogr -f GeoJSON -clipdst 115.837 -31.9365 115.891 -31.962 buildings.json shape/buildings.shp
 
 
 echo "topojson to combine the above into perth.json"
-topojson -o perth.json waterways.json roads.json
+topojson -o perth.json water.json parks.json buildings.json roads.json
 
 rm roads.json
-rm waterways.json
+rm parks.json
+rm water.json
+rm buildings.json
